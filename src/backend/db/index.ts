0bf2b9531017eedb790c34fbc0218164ef187a29
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-10-16 23:17:33 +0300

    code refactoring, adding migrations for development db

--- src/backend/db/index.ts
+++ src/backend/db/index.ts
@@ -4,20 +4,23 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
 import { drizzle as drizzleDev } from "drizzle-orm/postgres-js";
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-if (!process.env.NEON_DATABASE_URL) {
-  throw new Error("NEON_DATABASE_URL must be set as environment variable");
-}
 
-let database: ReturnType<typeof drizzle> | ReturnType<typeof drizzleDev>;
+const createProductionDb = () => {
+  if (!process.env.NEON_DATABASE_URL) {
+    throw new Error("NEON_DATABASE_URL must be set as environment variable");
+  }
+  const sql = neon(process.env.NEON_DATABASE_URL);
+  return drizzle(sql, { casing: "snake_case" });
+};
 
-if (process.env.NODE_ENV === "production") {
-  const sql = neon(process.env.NEON_DATABASE_URL)
-  database = drizzle(sql, { casing: "snake_case" });
-} else {
-  const queryClient = postgres("postgresql://username:password@localhost:5432/database");
-  database = drizzleDev(queryClient, { casing: "snake_case" });
-}
+const createDevelopmentDb = () => {
+  const sql = postgres("postgresql://username:password@localhost:5432/database");
+  const devDb = drizzleDev(sql, { casing: "snake_case" });
+  migrate(devDb, { migrationsFolder: "migrations" });
+  return devDb;
+};
 
-export const db = database;
+export const db = process.env.NODE_ENV === "production" ? createProductionDb() : createDevelopmentDb();
