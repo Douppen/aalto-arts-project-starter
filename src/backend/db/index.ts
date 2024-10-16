4a687807cbe410813b5e32825c3fd0f403d73ec1
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-10-16 22:34:55 +0300

    add local postgres

--- src/backend/db/index.ts
+++ src/backend/db/index.ts
@@ -3,10 +3,21 @@ import "server-only";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
+import { drizzle as drizzleDev } from "drizzle-orm/postgres-js";
+import postgres from 'postgres';
+
 if (!process.env.NEON_DATABASE_URL) {
   throw new Error("NEON_DATABASE_URL must be set as environment variable");
 }
 
-const sql = neon(process.env.NEON_DATABASE_URL);
+let database: ReturnType<typeof drizzle> | ReturnType<typeof drizzleDev>;
 
-export const db = drizzle(sql, { casing: "snake_case" });
+if (process.env.NODE_ENV === "production") {
+  const sql = neon(process.env.NEON_DATABASE_URL)
+  database = drizzle(sql, { casing: "snake_case" });
+} else {
+  const queryClient = postgres("postgresql://username:password@localhost:5432/database");
+  database = drizzleDev(queryClient, { casing: "snake_case" });
+}
+
+export const db = database;
