7009f00186f2cc3308053b5af35536947a973aa8
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-10-17 11:13:37 +0300

    readme additions, commenting out crashing code, newer postgres image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Card } from "@/app/components/card";
 import { CreateArtwork } from "./components/create-artwork";
 import { Artworks } from "./components/artworks";
-import { getPresignedUrl } from "@/backend/utils/get-presigned-url";
-import { UploadImage } from "./components/upload-image";
+//import { getPresignedUrl } from "@/backend/utils/get-presigned-url";
+//import { UploadImage } from "./components/upload-image";
 
 export default async function Home() {
-  const s3Url = await getPresignedUrl();
+  //const s3Url = await getPresignedUrl();
 
   return (
     <main className="flex flex-wrap gap-8">
@@ -16,7 +16,11 @@ export default async function Home() {
         <Artworks />
       </Card>
       <Card>
+        -
+        {/*
         <UploadImage url={s3Url} />
+        */
+        }
       </Card>
     </main>
   );
