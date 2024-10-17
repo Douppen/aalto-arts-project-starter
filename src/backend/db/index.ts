44abc22d2f89206214282e00ce194f096899c99d
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-10-17 11:30:04 +0300

    awaited migration

--- src/backend/db/index.ts
+++ src/backend/db/index.ts
@@ -16,10 +16,10 @@ const createProductionDb = () => {
   return drizzle(sql, { casing: "snake_case" });
 };
 
-const createDevelopmentDb = () => {
+const createDevelopmentDb = async () => {
   const sql = postgres("postgresql://username:password@localhost:5432/database");
   const devDb = drizzleDev(sql, { casing: "snake_case" });
-  migrate(devDb, { migrationsFolder: "migrations" });
+  await migrate(devDb, { migrationsFolder: "migrations" });
   return devDb;
 };
 
